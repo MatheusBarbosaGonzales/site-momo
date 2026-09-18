04fc96f531918f9ad25c488f12df47b1a6c6ba91
Matheus Gonzales <matheusbarbosa.gonzales@gmail.com>
2026-09-18 18:24:10 -0300

    correção pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import PaginaRomantica from "./pages/PaginaRomantica";
-import Gatuxa from "./pages/Gatuxa"
+import Gatuxa from "./pages/Gatuxa";
 
 const App = () => {
-    return(
-     <BrowserRouter>
+    return (
+        <BrowserRouter basename="/site-momo">
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/EuTeAmo" element={<PaginaRomantica/>} />
-                <Route path="/gatuxa" element={<Gatuxa/>} />
+                <Route path="/EuTeAmo" element={<PaginaRomantica />} />
+                <Route path="/gatuxa" element={<Gatuxa />} />
             </Routes>
         </BrowserRouter>
-        );
+    );
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,4 +10,5 @@ export default defineConfig({
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  base: '/site-momo/',
 })
